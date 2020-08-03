9ce9595484115aa43c3f281036072c943d4f1785
garvita <garvitaa@buffalo.edu>
2020-08-03 11:49:18 -0500

    removed global vector variable

--- plugins/jetVetoMap.h
+++ plugins/jetVetoMap.h
@@ -2,51 +2,33 @@
 #define __jetVetoMap_C__
 
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <map>
 #include <vector>
-#include <tuple>
 #include <utility>
 
 #include "TFile.h"
 #include "TH2D.h"
-#include "TMath.h"
-#include "TString.h"
 
 using namespace std;
 
-vector<vector<float>> JetVetoMap(4); 
-
-vector<vector<float>> getVetoMap() {
-  return JetVetoMap;
-}
-
-void jetVetoMap(TString filename="jetveto.root", TString map_name = "h2hot_ul18_plus_hem1516_and_hbp2m1" ) {
-  TDirectory *curdir = gDirectory;
-
-  TFile *f0 = new TFile(filename,"READ");
-  assert(f0 && !f0->IsZombie());
-  curdir->cd();
+vector<vector<float>> jetVetoMap(TString filename="jetvetoinput.root", TString map_name = "h2hot_ul18_plus_hem1516_and_hbp2m1" ) {
+  vector<vector<float>> m_JetVetoMap(4);
+  TFile *f0 = TFile::Open(filename);
+  if (!f0->IsOpen()){cout << "Jet Veto File not open .." <<endl;}
 
   TH2D *h2 = (TH2D*)f0->Get(map_name);
-  assert(h2);
-
+  m_JetVetoMap.clear();
   for (int i = 1; i != h2->GetNbinsX()+1; ++i) {
     for (int j = 1; j !=  h2->GetNbinsY()+1; ++j) {
       if (h2->GetBinContent(i,j) >0){
-        JetVetoMap[0].push_back(h2->GetXaxis()->GetBinLowEdge(i)); 
-        JetVetoMap[1].push_back(h2->GetXaxis()->GetBinLowEdge(i) + h2->GetXaxis()->GetBinWidth(i));
-        JetVetoMap[2].push_back(h2->GetYaxis()->GetBinLowEdge(j));
-        JetVetoMap[3].push_back(h2->GetYaxis()->GetBinLowEdge(j) + h2->GetYaxis()->GetBinWidth(j));
-
-        // std::cout<< "eta_low: " << h2->GetXaxis()->GetBinLowEdge(i) << " eta_high: " << (h2->GetXaxis()->GetBinLowEdge(i) + h2->GetXaxis()->GetBinWidth(i))
-        //          << " phi_low: " << h2->GetYaxis()->GetBinLowEdge(j) << " phi_high: " << (h2->GetYaxis()->GetBinLowEdge(j) + h2->GetYaxis()->GetBinWidth(j))
-        //          << "\t" <<h2->GetBinContent(i,j)<<std::endl;
-
+        m_JetVetoMap[0].push_back(h2->GetXaxis()->GetBinLowEdge(i)); 
+        m_JetVetoMap[1].push_back(h2->GetXaxis()->GetBinLowEdge(i) + h2->GetXaxis()->GetBinWidth(i));
+        m_JetVetoMap[2].push_back(h2->GetYaxis()->GetBinLowEdge(j));
+        m_JetVetoMap[3].push_back(h2->GetYaxis()->GetBinLowEdge(j) + h2->GetYaxis()->GetBinWidth(j));
       }
     }
   }
+  f0->Close();
+  return m_JetVetoMap;
 }
 
 #endif //__jetVetoMap_C__
